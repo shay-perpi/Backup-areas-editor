e1d297224f365ac479c0ea1687b6941ee269bbb9
shay-perpi <shayperp@rafael.co.il>
2024-01-07 10:37:14 +0200

    Back Up first version:1.0

---
+++ app.py
@@ -0,0 +1,150 @@
+# app.py (Streamlit application with centered layout)
+import geojson
+import boto3
+import streamlit as st
+import folium
+from streamlit_folium import folium_static
+import json
+from config import s3_credentials, s3_path_key, calculate_resolution_deg, validate_record_id, validate_footprint
+
+# Set your S3 credentials
+AWS_ACCESS_KEY = s3_credentials["s3_user"]
+AWS_SECRET_KEY = s3_credentials["s3_password"]
+S3_BUCKET = s3_credentials["s3_bucket"]
+S3_KEY = s3_path_key
+s3_endpoint = s3_credentials["s3_ip"]
+# Connect to S3
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY,
+                  endpoint_url=s3_endpoint)
+
+
+# Load JSON from S3
+def load_json_from_s3():
+    try:
+        response = s3.get_object(Bucket=S3_BUCKET, Key=S3_KEY)
+        data = json.loads(response['Body'].read())
+        return data
+    except Exception as e:
+        st.error(f"Error loading JSON from S3: {str(e)}")
+        return None
+
+
+# Save JSON to S3
+def save_json_to_s3(data):
+    try:
+        s3.put_object(Body=json.dumps(data, indent=2), Bucket=S3_BUCKET, Key=S3_KEY)
+        st.success("JSON saved successfully!")
+    except Exception as e:
+        st.error(f"Error saving JSON to S3: {str(e)}")
+        raise e
+
+
+def main():
+    st.set_page_config(
+        page_title="Export BackUP Handler",
+        page_icon="🌍",
+        layout="centered"
+    )
+
+    st.title("Backup areas editor")
+
+    # Load JSON from S3
+    json_data = load_json_from_s3()
+
+    # Display expander for Current JSON Data
+    with st.expander("Current JSON Data", expanded=False):
+        if json_data is not None:
+            # Display current JSON data
+            st.write("Current JSON Data:", json_data)
+
+    # Let the user choose between form and file upload
+    option = st.radio("Choose an option:", ("Add New Area (Form)", "Upload JSON File"))
+
+    if option == "Add New Area (Form)":
+        # Form for adding new areas
+        st.write("## Add New Area")
+        new_area_name = st.text_input("Name:")
+        new_area_footprint = st.text_area("Footprint (List of coordinates):", height=100)
+        new_area_record_id = st.text_input("Record ID:")
+
+        # Zoom level options
+        zoom_level_options = list(range(23))
+
+        # Display the zoom level dropdown
+        selected_zoom_level = st.selectbox("Area Zoom Level:", zoom_level_options)
+
+        if selected_zoom_level != "Choose...":
+            # Calculate resolutionDeg based on the selected zoom level
+            new_area_resolution_value = calculate_resolution_deg(selected_zoom_level)
+
+            st.write(f"Zoom Level: {selected_zoom_level}")
+            st.write(f"Resolution: {new_area_resolution_value}")
+
+            if st.button("Add Area"):
+                try:
+                    # Validate the record ID before adding the area
+                    if validate_record_id(new_area_record_id):
+                        new_area_resolution_value = calculate_resolution_deg(selected_zoom_level)
+                    else:
+                        st.error(f"Record ID '{new_area_record_id}' does not exist in the database.")
+                    if validate_footprint(new_area_footprint):
+                        new_area_resolution_value = calculate_resolution_deg(selected_zoom_level)
+                    else:
+                        st.error(f"Footprint does not valid.")
+
+                    new_area = {
+                        "name": new_area_name,
+                        "Footprint": new_area_footprint,
+                        "record_id": new_area_record_id,
+                        "resolutionDeg": new_area_resolution_value,
+                        "zoomlevel": selected_zoom_level
+                    }
+
+                    json_data['areas'].append(new_area)
+                    save_json_to_s3(json_data)
+
+                    st.success(f"Area '{new_area_name}' added successfully!")
+                except json.JSONDecodeError as e:
+                    st.error(f"Invalid JSON for Footprint: {str(e)}")
+
+    elif option == "Upload JSON File":
+        # File upload for adding new areas
+        uploaded_file = st.file_uploader("Upload JSON File", type=["json"])
+
+        if uploaded_file is not None:
+            try:
+                file_contents = uploaded_file.read()
+                new_areas = json.loads(file_contents)["areas"]
+
+                # Check for duplicates before adding new areas
+                if "areas" not in json_data:
+                    json_data["areas"] = []
+
+                for new_area in new_areas:
+                    # Check if the area already exists in json_data
+                    if new_area not in json_data["areas"]:
+                        json_data["areas"].append(new_area)
+                    else:
+                        st.warning(f"Duplicate area found: {new_area['name']}. It won't be added.")
+
+                # Save updated json_data to S3
+                save_json_to_s3(json_data)
+
+                st.success("Areas from the uploaded file added successfully!")
+
+            except json.JSONDecodeError as e:
+                st.error(f"Invalid JSON file: {str(e)}")
+
+    # Display areas for deletion
+    st.write("## Delete Area")
+    for area in json_data.get('areas', []):
+        if st.button(f"Delete {area['name']}"):
+            # Confirmation dialog
+            if st.warning(f"Are you sure you want to delete {area['name']}?"):
+                json_data['areas'].remove(area)
+                save_json_to_s3(json_data)
+                st.success(f"Area '{area['name']}' deleted successfully!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ config.py
@@ -0,0 +1,79 @@
+import os
+import geojson
+import psycopg2
+
+s3_path_key = os.getenv('path_key', 'backups/area_zones.json')
+s3_credentials = os.getenv("s3_credentials") or {
+    "s3_user": "automation",
+    "s3_password": "automationPassword",
+    "s3_bucket": "automation-team",
+    "s3_ip": "http://10.8.0.9:9000",
+    "s3_bucket_folder": ""
+}
+pg_credential = os.getenv("pg_credential") or{
+        "pg_host": "10.0.4.4",
+        "pg_user": "postgres",
+        "pg_port": "5432",
+        "pg_pass": "Libot4allnonprod",
+        "pg_job_task_table": "raster-integration",
+        "pg_pycsw_record_table": "raster-qa",
+        "pg_mapproxy_table": "raster-qa",
+        "pg_agent_table": "raster-qa"
+    }
+table_in_db = os.getenv("tableInDB",'"RasterCatalogManager".records')
+record_id_in_db =os.getenv("recordInDB", 'identifier')
+
+def validate_footprint(input_foot_print):
+    footprint_data = {
+    "coordinates": [input_foot_print]
+    }
+    try:
+        geojson.loads(footprint_data)
+        return True
+    except ValueError as e:
+        print(f"Footprint validation failed: {e}")
+        return False
+# Example usage without specifying the "type"
+
+
+
+def validate_record_id(record_id):
+    try:
+        # Replace these values with your PostgreSQL connection details
+        db_connection = psycopg2.connect(database=pg_credential["pg_job_task_table"],
+                        host=pg_credential["pg_host"],
+                        user=pg_credential["pg_user"],
+                        password=pg_credential["pg_pass"],
+                        port=pg_credential["pg_port"])
+        # Create a cursor
+        cursor = db_connection.cursor()
+
+        # Perform a query to check if the record ID exists
+        cursor.execute(f"SELECT COUNT(*) FROM {table_in_db} WHERE {record_id_in_db} = %s", (record_id,))
+        count = cursor.fetchone()[0]
+
+        # Close the cursor and connection
+        cursor.close()
+        db_connection.close()
+
+        return count > 0  # Returns True if the record ID exists, False otherwise
+    except Exception as e:
+        print(f"Error checking record ID in PostgreSQL: {str(e)}")
+        return False
+
+
+def calculate_resolution_deg(zoom_level):
+    resolutions = [
+        1.0, 0.3515625, 0.17578125, 0.087890625, 0.0439453125,
+        0.02197265625, 0.010986328125, 0.0054931640625, 0.00274658203125,
+        0.001373291015625, 0.0006866455078125, 0.00034332275390625,
+        0.000171661376953125, 0.0000858306884765625, 0.00004291534423828125,
+        0.000021457672119140625, 0.000010728836059570312, 0.000005364418029785156,
+        0.000002682209014892578, 0.000001341104507446289, 6.705522537231445e-7,
+        3.3527612686157227e-7, 1.6763806343078613e-7
+    ]
+
+    if 0 <= zoom_level <= 22:
+        return resolutions[zoom_level]
+    else:
+        return None
